378543b9c75c69bca70e36e6405471b0270fdf6f
erwinlo <erwin.luo@gmail.com>
2021-02-22 15:58:35 +0800

    Single Page Applications Part 1

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { faPhone, faFax, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  faPhone = faPhone;
+  faFax = faFax;
+  faEnvelope = faEnvelope;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
