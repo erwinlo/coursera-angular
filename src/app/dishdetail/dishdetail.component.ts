8d3c9f15c3e9eca569cce8371bef9cd74dcea491
erwinlo <erwin.luo@gmail.com>
2021-02-25 16:52:00 +0800

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -10,11 +10,25 @@ import {
   faChevronLeft,
   faChevronRight,
 } from '@fortawesome/free-solid-svg-icons';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({ transform: 'scale(1.0)', opacity: 1 })),
+      state('hidden', style({ transform: 'scale(0.5)', opacity: 0 })),
+      transition('* => *', animate('0.5s ease-in-out')),
+    ]),
+  ],
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
@@ -26,6 +40,7 @@ export class DishdetailComponent implements OnInit {
 
   faLeft = faChevronLeft;
   faRight = faChevronRight;
+  visibility = 'shown';
 
   newCommentForm: FormGroup;
   newComment: Comment;
@@ -67,13 +82,17 @@ export class DishdetailComponent implements OnInit {
       .subscribe((dishIds) => (this.dishIds = dishIds));
     this.route.params
       .pipe(
-        switchMap((params: Params) => this.dishService.getDish(params['id']))
+        switchMap((params: Params) => {
+          this.visibility = 'hidden';
+          return this.dishService.getDish(params['id']);
+        })
       )
       .subscribe(
         (dish) => {
           this.dish = dish;
           this.dishcopy = dish;
           this.setPrevNext(dish.id);
+          this.visibility = 'shown';
         },
         (errmess) => (this.errMess = <any>errmess)
       );
